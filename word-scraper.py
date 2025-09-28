fa485ffbaaba5a10953bc841145ab680c8a203c2
Krishna Mehta <kmehta81@gatech.edu>
2025-09-28 16:24:57 -0700

    removes tracking for unnecessary files

--- word-scraper.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("words.txt", "r", encoding="utf-8") as f:
-    words = f.read().split()
-
-with open("wordle_words.json", "w", encoding="utf-8") as f:
-    json.dump(words, f, indent=4)
